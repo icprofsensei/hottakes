a46e2ca8ef8320062d7e785841a4950cd5475a6f
icprofsensei <dhylan@live.co.uk>
2025-12-31 22:20:09 +0000

    landingpagesorted

--- generaluse/intro.js
+++ generaluse/intro.js
@@ -22,22 +22,19 @@ canvas = document.getElementById('lander');
 ctx = canvas.getContext('2d');
 setCanvasSize();
 
-let x = 0;
-let y = canvas.height / 4;
-let r = 5;
+let r = 1;
 
 const cx = canvas.width / 2;
-const cy = canvas.height / 3;
-const ri = canvas.width / 20;
-const randarr = [{ x: canvas.width / 5, y: canvas.height / 5}, { x: canvas.width / 7, y: canvas.height * 4 / 5},{ x: canvas.width *2 /3, y: canvas.height * 4 /5}]
+const cy = canvas.height /2;
+var ri = 25;
 let coordarray = [];
 
-for (let xo = 0; xo < canvas.width; xo+= 0.15) {
-    let inside = Math.pow(ri, 2) - Math.pow(xo - cx, 2);
+for (let xo = 0; xo < canvas.width; xo+= 0.01) {
+    let presqrt = Math.pow(ri, 2) - Math.pow(xo - cx, 2);
 
-    if (inside >= 0) {
-        let yTop = cy - Math.sqrt(inside);
-        let yBottom = cy + Math.sqrt(inside);
+    if (presqrt >= 0) {
+        let yTop = cy - Math.sqrt(presqrt);
+        let yBottom = cy + Math.sqrt(presqrt);
 
         coordarray.push({ x: xo, y: yTop });
         coordarray.push({ x: xo, y: yBottom });
@@ -46,26 +43,46 @@ for (let xo = 0; xo < canvas.width; xo+= 0.15) {
 
 let index = 0;
 
-function step() {
+setInterval(function() {
     let item = coordarray[index];
     let arraylen = coordarray.length
     animateCircle(item.x, item.y, r);
     if (index <= 30){index += 1;}
     else if (index == arraylen - 2){
-        drawCircle(cx, cy, ri);
-        for (let i = 0; i < randarr.length; i ++ ){
-            setTimeout(drawCircle(randarr[i].x, randarr[i].y, ri), 1500)
-        }
+        drawCircle(cx, cy, ri)
+        let vx = 0
+        let vy = 0
+        setInterval(function(){
+            drawCircle(cx + vx, cy+ vy, r);
+            drawCircle(cx + vx, cy- vy, r);
+            drawCircle(cx - vx, cy- vy, r);
+            drawCircle(cx - vx, cy+ vy, r);
+            vx += 5
+            vy += 5
+            console.log(vx, vy)
+            if ((cx - vx) <= 0 || (cx + vx) >= canvas.width ||
+             (cy + vy) >= canvas.height || (cy - vy) <= 0 ){
+                    setInterval(function(){
+                            drawCircle(cx, cy, ri)
+                            ri += 1
+                            if (ri >= canvas.width /2){
+                                window.location.replace("home.html");
+                            }
+                        }, ri+=0.1)
+            }
+            }, 5)
+
+            
     }
     else if (index > arraylen -30){ index += 1}
     
     else {
-        index += 10
+        index += 7
     }
-    requestAnimationFrame(step);
-}
+    
+},0.1
+)
 
-requestAnimationFrame(step);
 
 
 
